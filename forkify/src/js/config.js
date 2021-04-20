7ff67548ddcc48e0d7b640ee6eec3810b18fe2f6
Ishika Gupta <ishika4242@gmail.com>
2021-04-20 09:26:02 +0530

    Recipe App

---
+++ forkify/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'a8b98715-dff4-40b3-940f-16679e14800e';
+export const MODAL_CLOSE_SEC = 2.5;
